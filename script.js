f08e60040937257fdccb07f2c84e5c1f84fb9305
devsfabio <fabiio403@gmail.com>
2025-05-13 20:58:23 -0300

    atualizndo imagem e video

--- script.js
+++ script.js
@@ -14,13 +14,3 @@ window.addEventListener('scroll', function () {
   const header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 });
-
-// Adicionando classe sticky ao CSS
-const style = document.createElement('style');
-style.textContent = `
-  header.sticky {
-      background-color: rgba(255, 255, 255, 0.95);
-      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  }
-`;
-document.head.appendChild(style);
